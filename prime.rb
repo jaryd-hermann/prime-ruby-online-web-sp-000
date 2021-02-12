a8461957ba1bdc4f38646ae25da2b290e29b4286
Jaryd Hermann <hermannjaryd@gmail.com>
2021-02-12 12:07:12 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,16 @@
 # Add  code here!
 def prime?(number)
-  number.prime?(number)
+  if nunber == 1
+  return true 
+elsif number < 1
+  return false
+else
+for y in 2..(number-1)
+  if (number % y) == 0
+    return false 
+  end
+end 
+end
+true
+end
 end
